0fc8cf12015d56a188e6776741d036720b99f1c0
janiscirulis <125916978+janiscirulis@users.noreply.github.com>
2024-07-31 13:35:18 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "CiiAI"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Ko vēlies"
-    chat_description: str = "Esmu CiiAi un mēģināšu tev palīdzēt"
+    chat_description: str = "Esmu CiiAI un mēģināšu tev palīdzēt"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
